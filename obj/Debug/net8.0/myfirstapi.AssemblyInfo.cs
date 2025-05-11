8175be6281ce2d8124e420ab0ff50970c0fac05e
Rashedul Alam <rashed464@outlook.com>
2025-05-12 01:01:31 +0600

    first commit

--- obj/Debug/net8.0/myfirstapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/myfirstapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myfirstapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6032e0e72b203166b636fe1bf9b2bb0c7ebcdc1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cffc369e1b84e4d3b02991acdc033d03781f834a")]
 [assembly: System.Reflection.AssemblyProductAttribute("myfirstapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myfirstapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
